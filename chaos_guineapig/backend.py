c0a6e5081c14ccbf67f95662ca15fff10d0b0eb7
tony <alapenna@workit.fr>
2015-08-07 14:58:51 +0200

    Randomly shutdown a group of backend resources via the /off endpoint. Added a --time parameter to specify the period of time between each shutdown action. Close #1

--- chaos_guineapig/backend.py
+++ chaos_guineapig/backend.py
@@ -1,4 +1,12 @@
+from random import randint, sample
+from requests import get
+
 
 def gnaw_backend_resources(backend_resources):
-    print("Gnawww")
-    print(backend_resources)
+    targetedResourcesCount = randint(0, len(backend_resources))
+    print("Gnawwwing ", targetedResourcesCount, " resources.")
+    targetedResources = sample(backend_resources,
+                               targetedResourcesCount)
+    for resource in targetedResources:
+        print("GNAW:", resource)
+        r = get('http://' + resource + '/off')
--- chaos_guineapig/guineapig.py
+++ chaos_guineapig/guineapig.py
@@ -1,5 +1,6 @@
 import click
 from yaml import safe_load
+import time
 from .backend import gnaw_backend_resources
 
 
@@ -8,16 +9,26 @@ from .backend import gnaw_backend_resources
 @click.option('--backend',
               help="Gnaw the cables of a list of backend resources.",
               is_flag=True)
-def main(configuration_file, backend):
+@click.option('--time', default=10,
+              help='Maximum period of time between each action.',
+              type=int,
+              show_default=True)
+def main(configuration_file, backend, time):
     config = load_configuration(configuration_file)
     if backend:
         try:
             backend_resources = config["backend"]
-            gnaw_backend_resources(backend_resources)
         except KeyError:
             print("Missing backend section in configuration file.")
             exit(1)
+    start_chaos(time, backend_resources)
     exit(0)
+
+
+def start_chaos(t, backend_resources):
+    while True:
+        gnaw_backend_resources(backend_resources)
+        time.sleep(sleepTime)
 
 
 def load_configuration(configuration_file):
